f26c0f9fcb42d868c3e48f35a6fbad29f159b874
vincent zhao <vincentvolagu@gmail.com>
2022-04-28 20:31:02 +1000

    working draft version

--- math/square_number_seq.go
+++ math/square_number_seq.go
@@ -4,9 +4,133 @@ import "fmt"
 import "math"
 
 func main() {
-	fmt.Println("vim-go, 16! = ", factorial(16))
-	// bruteForce(16)
+	fmt.Println("vim-go")
+	n := 16
+	list := naturalOrderedList(n)
+	sn := squareNumbers(n)
+	fmt.Println("list is ", list)
+	fmt.Println("square numbers can be ", sn)
+	pairMap := make(map[int][]squarePair)
+	for _, v := range list {
+		pairMap[v] = findSquarePairs(v, list)
+	}
+	fmt.Println("map: ")
+	for _, v := range pairMap {
+		fmt.Println(v)
+	}
 
+	// navigate the graph until we can cover all points
+
+	for _, row := range pairMap {
+		for _, col := range row {
+			path := newPath()
+			path.add(col.source)
+			if depthFirstSearch(pairMap, col, path, n) {
+				fmt.Println("found path", path.nodes)
+				break
+			}
+		}
+	}
+}
+
+func depthFirstSearch(graph map[int][]squarePair, p squarePair, currentPath *path, depth int) bool {
+	edges, _ := graph[p.dest]
+	if !currentPath.add(p.dest) {
+		return false
+	}
+	if currentPath.len() == depth {
+		return true
+	}
+	// fmt.Println("looking at ", p)
+	// fmt.Println("looking ahead, path = ", currentPath.asString())
+	for _, v := range edges {
+		// fmt.Println("checking edge", v)
+		if v.connects(p) {
+			continue
+		}
+		// depth first search, when fail, backtrack and try next node
+		if depthFirstSearch(graph, v, currentPath, depth) {
+			return true
+		}
+	}
+	// fmt.Println("no path there, removing ", p.dest)
+	currentPath.remove(p.dest)
+	return false
+}
+
+type path struct {
+	nodes   []int
+	visited map[int]int
+}
+
+func newPath() *path {
+	return &path{visited: make(map[int]int)}
+}
+
+func (p *path) add(next int) bool {
+	// avoid acyclic path
+	if _, ok := p.visited[next]; !ok {
+		p.nodes = append(p.nodes, next)
+		p.visited[next] = len(p.nodes) - 1
+		return true
+	}
+	return false
+}
+
+func (p *path) remove(prev int) {
+	p.nodes = p.nodes[:len(p.nodes)-1]
+	delete(p.visited, prev)
+}
+
+func (p path) len() int {
+	return len(p.nodes)
+}
+
+func (p path) asString() string {
+	return fmt.Sprint(p.nodes)
+}
+
+type squarePair struct {
+	source, dest int
+}
+
+func (p squarePair) connects(other squarePair) bool {
+	return p.source == other.dest && p.dest == other.source
+}
+func (p squarePair) asKey() string {
+	return fmt.Sprint(p.source, "_", p.dest)
+}
+
+func findSquarePairs(n int, list []int) []squarePair {
+	var pairs []squarePair
+	for _, v := range list {
+		if v == n {
+			continue
+		}
+		if isSquareNumber(float64(n + v)) {
+			pairs = append(pairs, squarePair{n, v})
+		}
+	}
+	return pairs
+}
+
+func naturalOrderedList(n int) []int {
+	list := make([]int, n)
+	for i := 1; i <= n; i++ {
+		list[i-1] = i
+	}
+	return list
+}
+
+func squareNumbers(n int) []int {
+	maxSum := n + n - 1
+	var squareNums []int
+	for i := 1; i <= maxSum; i++ {
+		if isSquareNumber(float64(i)) {
+			squareNums = append(squareNums, i)
+		}
+	}
+	return squareNums
 }
 
 func bruteForce(n int) {
@@ -65,5 +189,6 @@ func verify(orderedList []int) bool {
 }
 
 func isSquareNumber(n float64) bool {
-	return math.Floor(math.Sqrt(n)) == n
+	sqrt := math.Sqrt(n)
+	return math.Floor(sqrt) == sqrt
 }
