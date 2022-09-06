3119d1a3e64010ba02b39da709ccdfcbf0fc8e77
vincent zhao <vincentvolagu@gmail.com>
2022-09-06 20:28:49 +1000

    add subgrid swapping

--- sudoku/main.go
+++ sudoku/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"reflect"
+	"sort"
 	"time"
 )
 
 func main() {
-	count := 0
-	// grid := newGrid(5)
-	// grid.Fill()
+	// grid := newGrid(4)
+	// grid = grid.Fill()
+	// fmt.Println("end result")
 	// grid.Print()
+	count := 0
 	for {
 		grid := newGrid(9)
-		grid.Fill()
+		grid = grid.Fill()
 		found := grid.Validate()
 
 		if found {
@@ -31,6 +35,12 @@ type Grid struct {
 	colBased [][]int
 }
 
+type subGrid struct {
+	mainGrid Grid
+
+	startRow, startCol, endRow, endCol int
+}
+
 func newGrid(size int) Grid {
 	return Grid{
 		size,
@@ -39,34 +49,40 @@ func newGrid(size int) Grid {
 	}
 }
 
+// the row/column is valid only it contains all the numbers from 1...n only once
+func (g Grid) isValidLine(line []int) bool {
+	expected := g.validRange()
+	actual := make([]int, g.size)
+	copy(actual, line)
+	sort.Ints(actual)
+	return reflect.DeepEqual(expected, actual)
+}
+
 func (g Grid) Validate() bool {
 	// do a simple version first, check no repeated number in any given row or column
 	for i := 0; i < g.size; i++ {
-		seen := make(map[int]bool)
-		for j := 0; j < g.size; j++ {
-			v := g.rowBased[i][j]
-			if v <= 0 || v > g.size {
-				return false
-			}
-			if _, exists := seen[v]; exists {
-				return false
-			}
-			seen[v] = true
+		if !g.isValidLine(g.rowBased[i]) && !g.isValidLine(g.colBased[i]) {
+			return false
 		}
 	}
-	for i := 0; i < g.size; i++ {
-		seen := make(map[int]bool)
-		for j := 0; j < g.size; j++ {
-			v := g.rowBased[j][i]
-			if v <= 0 || v > g.size {
-				return false
-			}
-			if _, exists := seen[v]; exists {
-				return false
-			}
-			seen[v] = true
-		}
-	}
+
+	// find smaller sub-grid and check numbers in that grid
+	// if subGridSize, hasSubGrid := g.subGridSize(); hasSubGrid {
+	// for i := 0; i < subGridSize; i++ {
+	// 	for j := 0; j < subGridSize; j++ {
+	// 		var sgList []int
+	// 		for k := 0; k < g.size; k++ {
+	// 			row := (i * subGridSize) + (k % subGridSize)
+	// 			col := (j * subGridSize) + (k % subGridSize)
+	// 			sgList = append(sgList, g.rowBased[row][col])
+	// 			if !g.isValidLine(sgList) {
+	// 				return false
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// }
+
 	return true
 }
 
@@ -86,19 +102,49 @@ func (g Grid) randOrderedList() []int {
 	return nr
 }
 
-func (g Grid) Fill() {
+func (g Grid) Fill() Grid {
 	// start with a randomised row 0
 	startingRow := g.randOrderedList()
 	for j := 0; j < g.size; j++ {
 		g.rowBased[0][j] = startingRow[j]
+		g.colBased[j][0] = startingRow[j]
 	}
 
 	// shift the 1st randomised row to populate the rest rows
 	for i := 1; i < g.size; i++ {
 		for j := 0; j < g.size; j++ {
 			g.rowBased[i][j] = startingRow[(i+j)%g.size]
+			g.colBased[j][i] = startingRow[(i+j)%g.size]
 		}
 	}
+
+	fmt.Println("after initial fill")
+	g.Print()
+
+	// swap rows so sub grids also match sequence
+	if subGridSize, hasSubGrid := g.subGridSize(); hasSubGrid {
+		clone := newGrid(g.size)
+		ci := 0
+		for i := 0; i < subGridSize; i++ {
+			for j := 0; j < subGridSize; j++ {
+				copy(clone.rowBased[ci], g.rowBased[i+j*subGridSize])
+				ci++
+			}
+		}
+		fmt.Println("after row swapping")
+		clone.Print()
+		return clone
+	}
+
+	return g
+}
+
+func (g Grid) subGridSize() (int, bool) {
+	sqrt := math.Sqrt(float64(g.size))
+	if math.Floor(sqrt) == sqrt {
+		return int(sqrt), true
+	}
+	return 0, false
 }
 
 func (g Grid) hasValue(needle int, haystack []int) bool {
